4a51c67f7befcef4258e104a436d44aba49eb7e7
Sheng14 <1336840941.com>
2021-06-11 14:20:54 +0800

    Jest的简单配置

--- lesson2/math.test.js
+++ lesson2/math.test.js
@@ -1,5 +1,6 @@
-const math = require('./math');
-const { add, minus } = math;
+/*const math = require('./math');
+const { add, minus } = math;*/
+import { add, minus  } from './math';
 
 test('测试加法', () => {
     expect(add(3, 3)).toBe(6);
@@ -8,3 +9,4 @@ test('测试加法', () => {
 test('测试减法', () => {
     expect(minus(4, 3)).toBe(1);
 })
+// https://www.zip118.com/324715482_1222/206427.html
