bfb1740e70e30637f1207c56ca90ec64411032c4
Sheng14 <1336840941.com>
2021-06-12 10:40:29 +0800

    Jest中的mock

---
+++ lesson2/demo.js
@@ -0,0 +1,13 @@
+import Axios from "axios";
+
+export const runCallback = (callback) => {
+    callback();
+}
+
+export const createObject = (classItem) => {
+    new classItem();
+}
+
+export const getData = () => {
+    return Axios.get('/api').then((res) => res.data);
+}
