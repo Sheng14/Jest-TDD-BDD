4cc27ef1bd0ab537c0c16e2c52430797925a9448
Sheng14 <1336840941.com>
2021-06-13 11:01:59 +0800

    Jest中对DOM节点操作的测试

--- lesson2/demo1.js
+++ lesson2/demo1.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import $ from 'jquery';
 
 export const fetchData = () => {
     return axios.get('/').then((res) => res.data);
@@ -6,4 +7,8 @@ export const fetchData = () => {
 
 export const getNumber = () => {
     return 123
+}
+
+export const addDiv = () => {
+    $('body').append('<div/>');
 }
--- lesson2/demo1.test.js
+++ lesson2/demo1.test.js
@@ -1,6 +1,7 @@
+import $ from 'jquery';
 jest.mock('./demo1');
 import { fetchData } from "./demo1";
-const { getNumber } = jest.requireActual('./demo1');
+const { getNumber, addDiv } = jest.requireActual('./demo1');
 
 test('测试获取数据', () => {
     return fetchData().then((data) => {
@@ -10,4 +11,10 @@ test('测试获取数据', () => {
 
 test('测试数字', () => {
     expect(getNumber()).toEqual(123);
+})
+
+test('测试DOM', () => {
+    addDiv();
+    addDiv();
+    expect($('body').find('div').length).toEqual(2);
 })
